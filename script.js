2c5d53f677e470855f2818280e2e317a66465589
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-09-03 11:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function renderTimer(){
+	let date=new Date();
+	document.getElementById("timer").innerHTML=date.toLocaleString();
+}
+setIntervel(()=>{renderTimer},1000)
