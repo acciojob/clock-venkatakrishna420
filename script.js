0750e7432fc034e7b3904a750b009332974e1794
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-09-03 14:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ function renderDate(){
 	let date=new Date();
 	document.getElementById("timer").innerHTML=date.toLocaleString();
 }
-setInterval(renderDate,1000)
+setInterval(()=>{renderDate()},1000)
